708938281bcc6eb49b196ab9480de9df5c46ff38
TShilongo <100943813+TShilongo@users.noreply.github.com>
2022-05-04 21:25:11 +0200

    Create ExtraHW.js
    Created space for answers.

---
+++ ExtraHW.js
@@ -0,0 +1,60 @@
+// Additional assignments for Day 4
+
+/* EXTRA 1
+ Write a piece of code for reverting an array.
+ es:
+ [1, 3, 5] ==> [5, 3, 1]
+*/
+
+console.log("\n-----------Extra 1 answers-----------\n")
+
+
+/* EXTRA 2
+ Write a piece of code for getting the maximum numerical value from an array.
+*/
+
+
+console.log("\n-----------Extra 2 answers-----------\n")
+
+
+/* EXTRA 3
+ Write a piece of code for getting the minimum numerical value from an array.
+*/
+console.log("\n-----------Extra 3 answers-----------\n")
+
+
+
+/* EXTRA 4
+ Write a piece of code for getting only even numerical values from an array.
+*/
+
+console.log("\n-----------Extra 4 answers-----------\n")
+
+/* EXTRA 5
+ Write a piece of code for deleting only even entries from an array.
+*/
+
+console.log("\n-----------Extra 5 answers-----------\n")
+
+/* EXTRA 6
+ Write a piece of code for removing all the vowels from a string.
+*/
+
+console.log("\n-----------Extra 6 answers-----------\n")
+
+/* EXTRA 7
+ Write a piece of code for increasing all the numerical values in a array by 1.
+*/
+
+console.log("\n-----------Extra 7 answers-----------\n")
+
+/* EXTRA 8 
+ Replace all the strings contained in an array with their length.
+ es.: ["strive", "is", "great"] => [6, 2, 5]
+*/
+
+console.log("\n-----------Extra 8 answers-----------\n")
+
+
+
+console.log("\n-----------!!END OF EXTRA ANSWERS!!-----------\n")
